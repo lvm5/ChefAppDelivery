18050d3bdb6b959e47834e515f7acb03d041e92c
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2023-11-09 19:34:57 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Leandro Morais on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
